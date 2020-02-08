31e517ab5b110a5320cdaa8d8a7e8ca15ce68741
Milan Bojic <bojic.job@gmail.com>
2020-02-08 22:53:04 +0100

    Init commit - App sceleton.

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent } from './app.component';
+import { appRoutingModule } from './app.routing';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { BookcaseComponent } from './bookcase/bookcase.component';
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatGridListModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        BookcaseComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        //fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Domain/Data/DbInitializer.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(BookcaseContext context)
+        {
+            //Remove EnsureCreated when data needs to be preserved - EnsureCreated create new empty db
+            //context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Books.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var bookbs = new Book[]
+            {
+                new Book{Name="Prva knjiga"},
+                new Book{Name="Druga knjiga"},
+                
+            };
+            foreach (Book b in bookbs)
+            {
+                context.Books.Add(b);
+            }
+            context.SaveChanges();
+
+            var users = new User[]
+            {
+                new User{FirstName="Prvi", LastName = "Korisnik"},
+                new User{FirstName="Drugi", LastName = "Korisnik"},
+
+            };
+            foreach (User u in users)
+            {
+                context.Users.Add(u);
+            }
+            context.SaveChanges();
+
+            var enrollments = new SavedBook[]
+            {
+                new SavedBook{UserId=1,BookId=1}
+            };
+            foreach (SavedBook e in enrollments)
+            {
+                context.SavedBooks.Add(e);
+            }
+            context.SaveChanges();
+        }
+    }
+}
